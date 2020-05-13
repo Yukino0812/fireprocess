ada61939545edd9e78d8faf85764a8647ce13fb8
Yukino0812 <Yukino0812@qq.com>
2020-05-13 18:57:47 +0800

    commit

--- src/test/java/me/yukino/fireprocess/model/CellPredictModelTest.java
+++ src/test/java/me/yukino/fireprocess/model/CellPredictModelTest.java
@@ -1,8 +1,5 @@
 package me.yukino.fireprocess.model;
 
-import me.yukino.fireprocess.sensor.MockSensorProcessor;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 /**
@@ -11,17 +8,5 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class CellPredictModelTest {
-
-    private static ICellPredictModel cellPredictModel;
-
-    @Autowired
-    private MockSensorProcessor mockSensorProcessor;
-
-    @Test
-    public void testPredict() {
-        mockSensorProcessor.mockAlertFire();
-        cellPredictModel = CellPredictModelProvider.getCellPredictModel();
-        cellPredictModel.predict(10 * 60 * 1000);
-    }
 
 }
