6b0433f2dbd0c22a12b4ab5a5d6854d86e78651e
Yukino0812 <Yukino0812@qq.com>
2020-05-16 16:23:32 +0800

    修改整体架构

--- src/main/java/me/yukino/fireprocess/controller/FireProcessController.java
+++ src/main/java/me/yukino/fireprocess/controller/FireProcessController.java
@@ -1,18 +1,26 @@
 package me.yukino.fireprocess.controller;
 
+import me.yukino.fireprocess.alarm.FireAlarmer;
 import me.yukino.fireprocess.model.CellPredictModelProvider;
 import me.yukino.fireprocess.model.ModelPredictMessageQueue;
 import me.yukino.fireprocess.sensor.MockSensorProcessor;
+import me.yukino.fireprocess.util.CellCoordinateConvertor;
 import me.yukino.fireprocess.util.DataUtil;
+import me.yukino.fireprocess.vo.BurningCellVo;
 import me.yukino.fireprocess.vo.ResponseVo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.constraints.NotNull;
 
 /**
  * @author Hoshiiro Yukino
  */
 
+@Validated
 @RestController
 public class FireProcessController {
 
@@ -31,6 +39,18 @@ public class FireProcessController {
         mockSensorProcessor.mockAlertFire();
         messageQueue.requirePredict();
         return new ResponseVo(0,"Mock success");
+    }
+
+    @PostMapping("/alarm/fire")
+    public ResponseVo manuallyAlarmFire(@RequestParam @NotNull Double x,
+                                        @RequestParam @NotNull Double y,
+                                        @RequestParam @NotNull Double z){
+        int cellX = CellCoordinateConvertor.toCellIndex(x);
+        int cellY = CellCoordinateConvertor.toCellIndex(y);
+        int cellZ = CellCoordinateConvertor.toCellIndex(z);
+        BurningCellVo burningCellVo = new BurningCellVo(cellX, cellY, cellZ);
+//        FireAlarmer.alarmFire(burningCellVo);
+        return new ResponseVo(0,"Manually alarm fire success");
     }
 
     @PostMapping("/model/reset")
